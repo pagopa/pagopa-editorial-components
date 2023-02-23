195355d2ee1ed23769c46431db60e6abd2a0c807
Ivan Di Luccio <ivan.diluccio@gmail.com>
2023-02-23 14:13:48 +0100

    feat(EDCO-1): added default mui-italia theme to stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,10 @@
+import React from 'react';
+import { createTheme, ThemeProvider } from "@mui/material";
+import { theme } from "@pagopa/mui-italia";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const mainTheme = createTheme({ ...theme });
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +14,12 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={mainTheme}>
+      <CssBaseline/>
+      <Story />
+    </ThemeProvider>
+  ),
+];
