8be7416381fcbc196cab09d42b530ead2cbaf4c4
Ivan Di Luccio <ivan.diluccio@gmail.com>
2023-02-23 20:07:44 +0100

    feat(EDCO-1): no cra needed

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,11 +6,7 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
+  "framework": "@storybook/react"
 }
--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/This project is designed to be used with storybook: run yarn storybook instead/i);
-  expect(linkElement).toBeInTheDocument();
-});
