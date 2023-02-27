979d91e383dc8f96dbc9a8d446c011cdbb563285
Ivan Di Luccio <ivan.diluccio@gmail.com>
2023-02-27 17:34:43 +0100

    feat(EDCO-1): prettier added

--- src/components/HelloWorldButton.tsx
+++ src/components/HelloWorldButton.tsx
@@ -1,12 +1,16 @@
-import { Button, type ButtonPropsVariantOverrides } from '@mui/material'
-import { type OverridableStringUnion } from '@mui/types'
+import { Button, type ButtonPropsVariantOverrides } from '@mui/material';
+import { type OverridableStringUnion } from '@mui/types';
 
 export interface HelloWorldButtonProps {
-  variant: OverridableStringUnion<'text' | 'outlined' | 'contained', ButtonPropsVariantOverrides>
-  text?: string
+  variant: OverridableStringUnion<
+    'text' | 'outlined' | 'contained',
+    ButtonPropsVariantOverrides
+  >;
+  text?: string;
 }
 
-const HelloWorldButton = (props: HelloWorldButtonProps) =>
-  <Button variant={props.variant}>{ (props.text != null) || 'Hello World'}</Button>
+const HelloWorldButton = (props: HelloWorldButtonProps) => (
+  <Button variant={props.variant}>{props.text != null || 'Hello World'}</Button>
+);
 
-export default HelloWorldButton
+export default HelloWorldButton;
--- src/stories/HelloWorldButton.stories.tsx
+++ src/stories/HelloWorldButton.stories.tsx
@@ -1,26 +1,28 @@
-import { type ComponentStory, type ComponentMeta } from '@storybook/react'
+import { type ComponentStory, type ComponentMeta } from '@storybook/react';
 
-import HelloWorldButton from '../components/HelloWorldButton'
+import HelloWorldButton from '../components/HelloWorldButton';
 
 export default {
   title: 'Hello World Button',
   component: HelloWorldButton,
-  argTypes: {}
-} as ComponentMeta<typeof HelloWorldButton>
+  argTypes: {},
+} as ComponentMeta<typeof HelloWorldButton>;
 
-const Template: ComponentStory<typeof HelloWorldButton> = (args) => <HelloWorldButton {...args} />
+const Template: ComponentStory<typeof HelloWorldButton> = (args) => (
+  <HelloWorldButton {...args} />
+);
 
-export const Text = Template.bind({})
+export const Text = Template.bind({});
 Text.args = {
-  variant: 'text'
-}
+  variant: 'text',
+};
 
-export const Outlined = Template.bind({})
+export const Outlined = Template.bind({});
 Outlined.args = {
-  variant: 'outlined'
-}
+  variant: 'outlined',
+};
 
-export const Contained = Template.bind({})
+export const Contained = Template.bind({});
 Contained.args = {
-  variant: 'contained'
-}
+  variant: 'contained',
+};
