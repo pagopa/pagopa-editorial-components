1dfef6694a899e623ec4d12f730728bfefe4d33a
Ivan Di Luccio <ivan.diluccio@gmail.com>
2023-02-27 12:04:36 +0100

    feat(EDCO-1): eslint and basic config added

--- src/components/HelloWorldButton.tsx
+++ src/components/HelloWorldButton.tsx
@@ -1,11 +1,12 @@
-import { Button, ButtonPropsVariantOverrides  } from '@mui/material';
-import { OverridableStringUnion } from '@mui/types';
+import { Button, type ButtonPropsVariantOverrides } from '@mui/material'
+import { type OverridableStringUnion } from '@mui/types'
 
 interface HelloWorldButtonProps {
   variant: OverridableStringUnion<'text' | 'outlined' | 'contained', ButtonPropsVariantOverrides>
   text?: string
 }
 
-const HelloWorldButton = (props: HelloWorldButtonProps) => <Button variant={props.variant}>{ props.text || 'Hello World'}</Button>;
+const HelloWorldButton = (props: HelloWorldButtonProps) =>
+  <Button variant={props.variant}>{ (props.text != null) || 'Hello World'}</Button>
 
-export default HelloWorldButton;
+export default HelloWorldButton
--- src/stories/HelloWorldButton.stories.tsx
+++ src/stories/HelloWorldButton.stories.tsx
@@ -1,29 +1,28 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { type ComponentStory, type ComponentMeta } from '@storybook/react'
 
-import HelloWorldButton from '../components/HelloWorldButton';
+import HelloWorldButton from '../components/HelloWorldButton'
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Hello World Button',
   component: HelloWorldButton,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {},
-} as ComponentMeta<typeof HelloWorldButton>;
+  argTypes: {}
+} as ComponentMeta<typeof HelloWorldButton>
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof HelloWorldButton> = (args) => <HelloWorldButton {...args} />;
+const Template: ComponentStory<typeof HelloWorldButton> = (args) => <HelloWorldButton {...args} />
 
-export const Text = Template.bind({});
+export const Text = Template.bind({})
 Text.args = {
-  variant: "text"
-};
+  variant: 'text'
+}
 
-export const Outlined = Template.bind({});
+export const Outlined = Template.bind({})
 Outlined.args = {
-  variant: "outlined"
-};
+  variant: 'outlined'
+}
 
-export const Contained = Template.bind({});
+export const Contained = Template.bind({})
 Contained.args = {
-  variant: "contained"
-};
+  variant: 'contained'
+}
