005cc5761b87a03a7410d1a22bb5441053274eff
Michele Moio <michele.moio@protonmail.com>
2023-03-15 17:43:07 +0100

    stories: dark and light editorial stories

---
+++ src/stories/Editorial/Defaults.ts
@@ -0,0 +1,19 @@
+import regular from '../../assets/Editorial/regular.png';
+
+export const defaults = {
+  args: {
+    theme: 'light',
+    image: { src: regular, alt: 'a landscape photo' },
+    eyelet: 'per i cittadini',
+    title: 'Non perderti più nessuna notifica',
+    body: "Con Piattaforma Notifiche, ricevi e gestisci nello stesso spazio tutti gli atti di notifica che ti inviano Enti e Pubbliche Amministrazioni, come multe o certificati elettorali. E, grazie all'integrazione con pagoPA, puoi anche pagare eventuali costi. Così, risparmi tempo e denaro.",
+    ctaButtons: [
+      {
+        text: 'Accedi',
+      },
+      {
+        text: 'Scopri di più',
+      },
+    ],
+  },
+};
--- src/stories/Editorial/dark.stories.tsx
+++ src/stories/Editorial/dark.stories.tsx
@@ -1,41 +1,31 @@
-import { type ComponentStory, type ComponentMeta } from '@storybook/react';
-import { Editorial, type EditorialProps } from '../components/Editorial';
-import regular from '../assets/Editorial/regular.png';
-import portrait from '../assets/Editorial/portrait.png';
 import Box from '@mui/material/Box';
+import { type ComponentMeta, type ComponentStory } from '@storybook/react';
+import portrait from '../../assets/Editorial/portrait.png';
+import { Editorial } from '../../components/Editorial';
+import { defaults } from './Defaults';
 
-const ctaButtons: EditorialProps['ctaButtons'] = [
-  {
-    text: 'Accedi',
-  },
-  {
-    text: 'Scopri di più',
-  },
-];
-
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
 export default {
   component: Editorial,
-  title: 'Editorial',
+  title: 'Editorial/Dark',
   argTypes: {},
-  args: {
-    theme: 'light',
-    image: { src: regular, alt: 'a landscape photo' },
-    eyelet: 'per i cittadini',
-    title: 'Non perderti più nessuna notifica',
-    body: "Con Piattaforma Notifiche, ricevi e gestisci nello stesso spazio tutti gli atti di notifica che ti inviano Enti e Pubbliche Amministrazioni, come multe o certificati elettorali. E, grazie all'integrazione con pagoPA, puoi anche pagare eventuali costi. Così, risparmi tempo e denaro.",
-    ctaButtons,
-  },
+  args: { ...defaults.args, theme: 'dark' },
 } as ComponentMeta<typeof Editorial>;
 
 const Template: ComponentStory<typeof Editorial> = (args) => (
   <Editorial {...args} />
 );
 
-export const Light = Template.bind({});
+export const Standard = Template.bind({});
 
-export const Dark = Template.bind({});
-Dark.args = {
-  theme: 'dark',
+export const Wide = Template.bind({});
+Wide.args = {
+  width: 'wide',
+};
+
+export const Center = Template.bind({});
+Center.args = {
+  width: 'center',
 };
 
 export const Reversed = Template.bind({});
@@ -57,21 +47,17 @@ WithoutLabel.args = {
 export const Dots = Template.bind({});
 Dots.args = {
   pattern: 'dots',
-  theme: 'dark',
-  image: { src: portrait as string, alt: 'a portrait photo' },
   width: 'standard',
 };
 
 export const Solid = Template.bind({});
 Solid.args = {
   pattern: 'solid',
-  reversed: true,
 };
 
 export const BoldBodyElementExample = Template.bind({});
 BoldBodyElementExample.args = {
-  pattern: 'solid',
-  reversed: true,
+  pattern: 'dots',
   body: (
     <p>
       Con Piattaforma Notifiche, ricevi e gestisci nello stesso spazio tutti gli
@@ -84,7 +70,6 @@ BoldBodyElementExample.args = {
 
 export const LabelImageElementExample = Template.bind({});
 LabelImageElementExample.args = {
-  pattern: 'none',
   image: (
     <Box>
       <img src={portrait} alt="another portrait" />
@@ -96,6 +81,8 @@ LabelImageElementExample.args = {
 export const RotatedImageElementExample = Template.bind({});
 RotatedImageElementExample.args = {
   pattern: 'solid',
+  width: 'wide',
+  reversed: true,
   image: (
     <Box style={{ transform: 'rotate(45deg) scale(0.8)' }}>
       <img src={portrait} alt="another portrait" />
