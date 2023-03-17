1338914e9af81ce9e03057d19c56c61c248bcab7
leleOFA <emanuele.didonato@pagopa.it>
2023-03-17 16:34:57 +0100

    fix: split feature stories in dark and light and rename title

--- src/stories/Feature/dark.stories.tsx
+++ src/stories/Feature/dark.stories.tsx
@@ -47,11 +47,7 @@ const itemMockWithoutLink = [
   },
 ];
 export default {
-  /* 👇 The title prop is optional.
-   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-   * to learn how to generate automatic titles
-   */
-  title: 'Feature',
+  title: 'Feature/dark',
   args: {
     title: 'Title',
     items: itemMockWithLink,
