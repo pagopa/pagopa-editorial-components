9c9f4fbeed71fb8953ce3b59bc95f267e28a8809
leleOFA <emanuele.didonato@pagopa.it>
2023-03-21 16:41:56 +0100

    fix: adpted width for footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -79,124 +79,73 @@ export const Footer = ({
   };
 
   return (
-    <Container>
-      <Stack>
-        <Box
-          sx={{
-            borderTop: 1,
-            borderColor: 'divider',
-            backgroundColor: 'background.paper',
-          }}
-        >
-          <Container maxWidth={false} sx={{ py: 8 }}>
-            <Grid container spacing={{ xs: 6, sm: 3 }}>
-              <Grid item xs={12} sm={3}>
-                <Stack spacing={2} alignItems={{ xs: 'center', sm: 'start' }}>
-                  {companyLink && (
-                    <Link
-                      component="button"
-                      aria-label={companyLink.ariaLabel}
-                      href={companyLink.href ?? hrefNoOp}
-                      onClick={wrapHandleExitAction(
-                        companyLink.href ?? hrefNoOp,
-                        companyLink.onClick,
-                        onExit
-                      )}
-                      sx={{ display: 'inline-flex' }}
-                    >
-                      <LogoPagoPACompany />
-                    </Link>
+    <Box
+      sx={{
+        borderTop: 1,
+        borderColor: 'divider',
+        backgroundColor: 'background.paper',
+      }}
+    >
+      <Container maxWidth={false} sx={{ py: 8 }}>
+        <Grid container spacing={{ xs: 6, sm: 3 }}>
+          <Grid item xs={12} sm={3}>
+            <Stack spacing={2} alignItems={{ xs: 'center', sm: 'start' }}>
+              {companyLink && (
+                <Link
+                  component="button"
+                  aria-label={companyLink.ariaLabel}
+                  href={companyLink.href ?? hrefNoOp}
+                  onClick={wrapHandleExitAction(
+                    companyLink.href ?? hrefNoOp,
+                    companyLink.onClick,
+                    onExit
                   )}
-
-                  <Stack
-                    component="ul"
-                    alignItems={{ xs: 'center', sm: 'start' }}
-                    sx={{ padding: 0, listStyle: 'none' }}
-                  >
-                    {aboutUs?.links.map(
-                      ({ href = hrefNoOp, label, ariaLabel, onClick }, i) => (
-                        <li key={i}>
-                          <Link
-                            aria-label={ariaLabel}
-                            component="a"
-                            href={href}
-                            onClick={wrapHandleExitAction(
-                              href,
-                              onClick,
-                              onExit
-                            )}
-                            underline="none"
-                            color="text.primary"
-                            sx={{ display: 'inline-block', py: 0.5 }}
-                            variant="subtitle2"
-                          >
-                            {label}
-                          </Link>
-                        </li>
-                      )
-                    )}
-                  </Stack>
-                </Stack>
-              </Grid>
-              {!hideProductsColumn && (
-                <Grid item xs={12} sm={3}>
-                  <Stack spacing={2} alignItems={{ xs: 'center', sm: 'start' }}>
-                    {jsonProducts && (
-                      <Typography variant="overline">
-                        Prodotti e Servizi
-                      </Typography>
-                    )}
-
-                    <Stack
-                      component="ul"
-                      alignItems={{ xs: 'center', sm: 'start' }}
-                      sx={{ padding: 0, listStyle: 'none' }}
-                    >
-                      {jsonProducts &&
-                        jsonProducts?.map(
-                          (
-                            { href = hrefNoOp, label, ariaLabel, onClick },
-                            i
-                          ) => (
-                            <li key={i}>
-                              <Link
-                                aria-label={ariaLabel}
-                                component="a"
-                                href={href}
-                                onClick={wrapHandleExitAction(
-                                  href,
-                                  onClick,
-                                  onExit
-                                )}
-                                underline="none"
-                                color="text.primary"
-                                sx={{ display: 'inline-block', py: 0.5 }}
-                                variant="subtitle2"
-                              >
-                                {label}
-                              </Link>
-                            </li>
-                          )
-                        )}
-                    </Stack>
-                  </Stack>
-                </Grid>
+                  sx={{ display: 'inline-flex' }}
+                >
+                  <LogoPagoPACompany />
+                </Link>
               )}
 
-              <Grid item xs={12} sm={3}>
-                <Stack spacing={2} alignItems={{ xs: 'center', sm: 'start' }}>
-                  {resources?.title && (
-                    <Typography variant="overline">
-                      {resources.title}
-                    </Typography>
-                  )}
+              <Stack
+                component="ul"
+                alignItems={{ xs: 'center', sm: 'start' }}
+                sx={{ padding: 0, listStyle: 'none' }}
+              >
+                {aboutUs?.links.map(
+                  ({ href = hrefNoOp, label, ariaLabel, onClick }, i) => (
+                    <li key={i}>
+                      <Link
+                        aria-label={ariaLabel}
+                        component="a"
+                        href={href}
+                        onClick={wrapHandleExitAction(href, onClick, onExit)}
+                        underline="none"
+                        color="text.primary"
+                        sx={{ display: 'inline-block', py: 0.5 }}
+                        variant="subtitle2"
+                      >
+                        {label}
+                      </Link>
+                    </li>
+                  )
+                )}
+              </Stack>
+            </Stack>
+          </Grid>
+          {!hideProductsColumn && (
+            <Grid item xs={12} sm={3}>
+              <Stack spacing={2} alignItems={{ xs: 'center', sm: 'start' }}>
+                {jsonProducts && (
+                  <Typography variant="overline">Prodotti e Servizi</Typography>
+                )}
 
-                  <Stack
-                    component="ul"
-                    alignItems={{ xs: 'center', sm: 'start' }}
-                    sx={{ padding: 0, listStyle: 'none' }}
-                  >
-                    {resources?.links.map(
+                <Stack
+                  component="ul"
+                  alignItems={{ xs: 'center', sm: 'start' }}
+                  sx={{ padding: 0, listStyle: 'none' }}
+                >
+                  {jsonProducts &&
+                    jsonProducts?.map(
                       ({ href = hrefNoOp, label, ariaLabel, onClick }, i) => (
                         <li key={i}>
                           <Link
@@ -218,119 +167,143 @@ export const Footer = ({
                         </li>
                       )
                     )}
-                  </Stack>
                 </Stack>
-              </Grid>
-
-              <Grid item xs={12} sm={3}>
-                <Stack
-                  spacing={3}
-                  justifyContent="space-between"
-                  sx={{ height: '100%' }}
-                  alignItems={{ xs: 'center', sm: 'start' }}
-                >
-                  <Stack spacing={2} alignItems={{ xs: 'center', sm: 'start' }}>
-                    {followUs?.title && (
-                      <Typography variant="overline">
-                        {followUs.title}
-                      </Typography>
-                    )}
-
-                    <Stack alignItems={{ xs: 'center', sm: 'start' }}>
-                      <Stack
-                        spacing={{ xs: 3, sm: 1.5, lg: 3 }}
-                        direction="row"
-                        component="ul"
-                        alignItems={{ xs: 'center', sm: 'start' }}
-                        sx={{ padding: 0, mt: 0.5, listStyle: 'none' }}
-                      >
-                        {followUs?.socialLinks.map(
-                          (
-                            { icon, href = hrefNoOp, ariaLabel, onClick },
-                            i
-                          ) => (
-                            <li key={i}>
-                              <Link
-                                aria-label={ariaLabel}
-                                component="button"
-                                href={href}
-                                onClick={wrapHandleExitAction(
-                                  href,
-                                  onClick,
-                                  onExit
-                                )}
-                                underline="none"
-                                color="text.primary"
-                                sx={{ display: 'inline-flex' }}
-                                variant="caption"
-                              >
-                                {icon && iconMap[icon]}
-                              </Link>
-                            </li>
-                          )
-                        )}
-                      </Stack>
-
-                      <Stack
-                        component="ul"
-                        alignItems={{ xs: 'center', sm: 'start' }}
-                        sx={{ padding: 0, margin: 0, listStyle: 'none' }}
-                      >
-                        {followUs?.links.map(
-                          (
-                            { href = hrefNoOp, label, ariaLabel, onClick },
-                            i
-                          ) => (
-                            <li key={i}>
-                              <Link
-                                aria-label={ariaLabel}
-                                component="button"
-                                href={href}
-                                onClick={wrapHandleExitAction(
-                                  href,
-                                  onClick,
-                                  onExit
-                                )}
-                                underline="none"
-                                color="text.primary"
-                                sx={{ display: 'inline-block', py: 0.5 }}
-                                variant="subtitle2"
-                              >
-                                {label}
-                              </Link>
-                            </li>
-                          )
-                        )}
-                      </Stack>
-
-                      <LangSwitch {...langProps} />
-                    </Stack>
-                  </Stack>
-                  <FundedByNextGenerationEU size={180} />
-                </Stack>
-              </Grid>
+              </Stack>
             </Grid>
-          </Container>
-        </Box>
-        <Box
-          sx={{
-            borderTop: 1,
-            borderColor: 'divider',
-            backgroundColor: 'background.paper',
-          }}
-        >
-          <Container sx={{ px: 2, py: 2 }}>
-            <Typography
-              color="text.primary"
-              component="p"
-              variant="caption"
-              sx={{ textAlign: 'center' }}
+          )}
+
+          <Grid item xs={12} sm={3}>
+            <Stack spacing={2} alignItems={{ xs: 'center', sm: 'start' }}>
+              {resources?.title && (
+                <Typography variant="overline">{resources.title}</Typography>
+              )}
+
+              <Stack
+                component="ul"
+                alignItems={{ xs: 'center', sm: 'start' }}
+                sx={{ padding: 0, listStyle: 'none' }}
+              >
+                {resources?.links.map(
+                  ({ href = hrefNoOp, label, ariaLabel, onClick }, i) => (
+                    <li key={i}>
+                      <Link
+                        aria-label={ariaLabel}
+                        component="a"
+                        href={href}
+                        onClick={wrapHandleExitAction(href, onClick, onExit)}
+                        underline="none"
+                        color="text.primary"
+                        sx={{ display: 'inline-block', py: 0.5 }}
+                        variant="subtitle2"
+                      >
+                        {label}
+                      </Link>
+                    </li>
+                  )
+                )}
+              </Stack>
+            </Stack>
+          </Grid>
+
+          <Grid item xs={12} sm={3}>
+            <Stack
+              spacing={3}
+              justifyContent="space-between"
+              sx={{ height: '100%' }}
+              alignItems={{ xs: 'center', sm: 'start' }}
             >
-              {legalInfo}
-            </Typography>
-          </Container>
-        </Box>
-      </Stack>
-    </Container>
+              <Stack spacing={2} alignItems={{ xs: 'center', sm: 'start' }}>
+                {followUs?.title && (
+                  <Typography variant="overline">{followUs.title}</Typography>
+                )}
+
+                <Stack alignItems={{ xs: 'center', sm: 'start' }}>
+                  <Stack
+                    spacing={{ xs: 3, sm: 1.5, lg: 3 }}
+                    direction="row"
+                    component="ul"
+                    alignItems={{ xs: 'center', sm: 'start' }}
+                    sx={{ padding: 0, mt: 0.5, listStyle: 'none' }}
+                  >
+                    {followUs?.socialLinks.map(
+                      ({ icon, href = hrefNoOp, ariaLabel, onClick }, i) => (
+                        <li key={i}>
+                          <Link
+                            aria-label={ariaLabel}
+                            component="button"
+                            href={href}
+                            onClick={wrapHandleExitAction(
+                              href,
+                              onClick,
+                              onExit
+                            )}
+                            underline="none"
+                            color="text.primary"
+                            sx={{ display: 'inline-flex' }}
+                            variant="caption"
+                          >
+                            {icon && iconMap[icon]}
+                          </Link>
+                        </li>
+                      )
+                    )}
+                  </Stack>
+
+                  <Stack
+                    component="ul"
+                    alignItems={{ xs: 'center', sm: 'start' }}
+                    sx={{ padding: 0, margin: 0, listStyle: 'none' }}
+                  >
+                    {followUs?.links.map(
+                      ({ href = hrefNoOp, label, ariaLabel, onClick }, i) => (
+                        <li key={i}>
+                          <Link
+                            aria-label={ariaLabel}
+                            component="button"
+                            href={href}
+                            onClick={wrapHandleExitAction(
+                              href,
+                              onClick,
+                              onExit
+                            )}
+                            underline="none"
+                            color="text.primary"
+                            sx={{ display: 'inline-block', py: 0.5 }}
+                            variant="subtitle2"
+                          >
+                            {label}
+                          </Link>
+                        </li>
+                      )
+                    )}
+                  </Stack>
+
+                  <LangSwitch {...langProps} />
+                </Stack>
+              </Stack>
+              <FundedByNextGenerationEU size={180} />
+            </Stack>
+          </Grid>
+        </Grid>
+      </Container>
+      <Box
+        sx={{
+          borderTop: 1,
+          borderColor: 'divider',
+          backgroundColor: 'background.paper',
+        }}
+      >
+        <Container sx={{ px: 2, py: 2 }}>
+          <Typography
+            color="text.primary"
+            component="p"
+            variant="caption"
+            sx={{ textAlign: 'center' }}
+          >
+            {legalInfo}
+          </Typography>
+        </Container>
+      </Box>
+    </Box>
   );
 };
