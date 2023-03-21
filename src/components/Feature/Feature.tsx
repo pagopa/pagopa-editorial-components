4e2bcff1a9134976656ec8bdae639880f1118497
leleOFA <emanuele.didonato@pagopa.it>
2023-03-21 11:47:04 +0100

    fix: fix style padding in feature component

--- src/components/Feature/Feature.tsx
+++ src/components/Feature/Feature.tsx
@@ -38,107 +38,103 @@ const Feature = ({ title, items, theme, showCarouselMobile }: FeatureProps) => {
   };
 
   return !showCarouselMobile ? (
-    <Container>
-      <Box bgcolor={theme === 'light' ? 'background.paper' : 'primary.main'}>
-        <Container
-          maxWidth="xl"
-          sx={{
-            py: {
-              xs: 4,
-              sm: 4,
-              md: 8,
-            },
-          }}
-        >
-          <Stack alignContent="center" textAlign="center" spacing={8}>
-            <Typography
-              variant="h4"
-              color={theme === 'light' ? 'text.primary' : 'background.paper'}
-            >
-              {title}
-            </Typography>
-            <Box
-              sx={{
-                display: 'grid',
-                gap: 3,
-                gridTemplateColumns: 'repeat(12, 1fr)',
-              }}
-            >
-              <Box gridColumn={{ xs: '2 / span 8', md: '2 / span 10' }}>
-                <Stack
-                  direction={{ xs: 'column', md: 'row' }}
-                  alignContent="center"
-                  justifyContent="center"
-                  spacing={{ xs: 6, md: 4 }}
-                >
-                  {items.map((item, index) => (
-                    <Stack
-                      key={index}
-                      alignContent="center"
-                      justifyContent="flex-start"
-                      spacing={{ xs: 1, md: 3 }}
+    <Box bgcolor={theme === 'light' ? 'background.paper' : 'primary.main'}>
+      <Container
+        maxWidth="xl"
+        sx={{
+          py: {
+            xs: 4,
+            sm: 4,
+            md: 8,
+          },
+        }}
+      >
+        <Stack alignContent="center" textAlign="center" spacing={8}>
+          <Typography
+            variant="h4"
+            color={theme === 'light' ? 'text.primary' : 'background.paper'}
+          >
+            {title}
+          </Typography>
+          <Box
+            sx={{
+              display: 'grid',
+              gap: 3,
+              gridTemplateColumns: 'repeat(12, 1fr)',
+            }}
+          >
+            <Box gridColumn={{ xs: '2 / span 10' }}>
+              <Stack
+                direction={{ xs: 'column', md: 'row' }}
+                alignContent="center"
+                justifyContent="center"
+                spacing={{ xs: 6, md: 4 }}
+              >
+                {items.map((item, index) => (
+                  <Stack
+                    key={index}
+                    alignContent="center"
+                    justifyContent="flex-start"
+                    spacing={{ xs: 1, md: 3 }}
+                    sx={{
+                      flex: 1,
+                    }}
+                  >
+                    <Box
+                      mx="auto"
                       sx={{
-                        flex: 1,
+                        svg: {
+                          height: '64px',
+                          width: '64px',
+                        },
                       }}
+                      color={
+                        theme === 'light' ? 'primary.main' : 'background.paper'
+                      }
                     >
-                      <Box
-                        mx="auto"
-                        sx={{
-                          svg: {
-                            height: '64px',
-                            width: '64px',
-                          },
-                        }}
+                      {item.icon}
+                    </Box>
+                    <Stack spacing={1}>
+                      <Typography
                         color={
                           theme === 'light'
-                            ? 'primary.main'
+                            ? 'text.primary'
                             : 'background.paper'
                         }
+                        variant="h6"
                       >
-                        {item.icon}
-                      </Box>
-                      <Stack spacing={1}>
-                        <Typography
-                          color={
-                            theme === 'light'
-                              ? 'text.primary'
-                              : 'background.paper'
-                          }
-                          variant="h6"
-                        >
-                          {item.title}
-                        </Typography>
-                        <>
-                          {typeof item.linkTitle === undefined ? (
-                            <Typography
-                              variant="body2"
-                              color={
-                                theme === 'light'
-                                  ? 'text.primary'
-                                  : 'background.paper'
-                              }
-                            >
-                              {item.subtitle}
-                            </Typography>
-                          ) : (
-                            <Subtitle
-                              theme={theme}
-                              subtitle={item.subtitle}
-                              textLink={item.linkTitle}
-                              url={item.url}
-                            ></Subtitle>
-                          )}
-                        </>
-                      </Stack>
+                        {item.title}
+                      </Typography>
+                      <>
+                        {typeof item.linkTitle === undefined ? (
+                          <Typography
+                            variant="body2"
+                            color={
+                              theme === 'light'
+                                ? 'text.primary'
+                                : 'background.paper'
+                            }
+                          >
+                            {item.subtitle}
+                          </Typography>
+                        ) : (
+                          <Subtitle
+                            theme={theme}
+                            subtitle={item.subtitle}
+                            textLink={item.linkTitle}
+                            url={item.url}
+                          ></Subtitle>
+                        )}
+                      </>
                     </Stack>
-                  ))}
-                </Stack>
-              </Box>
+                  </Stack>
+                ))}
+              </Stack>
             </Box>
-          </Stack>
-        </Container>
-      </Box>
-    </Container>
+          </Box>
+        </Stack>
+      </Container>
+    </Box>
   ) : (
     <Box
       bgcolor={theme === 'light' ? 'background.paper' : 'primary.main'}
