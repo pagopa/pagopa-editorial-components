11879adb66bda5f120dd00ba086e5cd613a6ea46
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2023-03-21 17:42:14 +0100

    feat(EDCO-21): PhotoVideo component

---
+++ src/stories/PhotoVideo/photoVideo.stories.tsx
@@ -0,0 +1,47 @@
+import { type ComponentStory, type ComponentMeta } from '@storybook/react';
+import PhotoVideo from '../../components/PhotoVideo';
+import image from '../assets/hero_background.jpg';
+
+export default {
+  title: 'PhotoVideo',
+  component: PhotoVideo,
+} as ComponentMeta<typeof PhotoVideo>;
+
+const Template: ComponentStory<typeof PhotoVideo> = (args) => (
+  <PhotoVideo {...args} />
+);
+
+export const Regular = Template.bind({});
+Regular.args = {
+  title: 'Nam eget dui. Etiam rhoncus Maecenas tempus',
+  subtitle:
+    'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+  src: 'https://assets.mixkit.co/videos/preview/mixkit-person-typing-on-a-computer-in-detail-4907-large.mp4',
+};
+
+export const Minimal = Template.bind({});
+Minimal.args = {
+  src: 'https://assets.mixkit.co/videos/preview/mixkit-person-typing-on-a-computer-in-detail-4907-large.mp4',
+};
+
+export const Full = Template.bind({});
+Full.args = {
+  title: 'Nam eget dui. Etiam rhoncus Maecenas tempus',
+  subtitle:
+    'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+  src: 'https://assets.mixkit.co/videos/preview/mixkit-person-typing-on-a-computer-in-detail-4907-large.mp4',
+  caption:
+    'Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  captionAlign: 'left',
+};
+
+export const Image = Template.bind({});
+Image.args = {
+  title: 'Nam eget dui. Etiam rhoncus Maecenas tempus',
+  subtitle:
+    'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+  src: image,
+  caption:
+    'Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  captionAlign: 'left',
+};
