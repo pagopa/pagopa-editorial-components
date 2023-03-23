0160d470424d8e3d65f2c1b9179ed28eb7dc423b
Vito Falco <vitofalco@gmail.com>
2023-03-23 12:36:41 +0100

    Fix padding

--- src/components/Feature/Feature.tsx
+++ src/components/Feature/Feature.tsx
@@ -144,7 +144,7 @@ const Feature = ({ title, items, theme, showCarouselMobile }: FeatureProps) => {
         justifyContent="space-evenly"
         alignItems="center"
         spacing={3}
-        px={3}
+        p={3}
       >
         <AutoPlaySwipeableViews
           axis={themeComponent.direction === 'rtl' ? 'x-reverse' : 'x'}
