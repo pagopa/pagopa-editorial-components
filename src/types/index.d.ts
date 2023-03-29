7d2d3bb9b8ebaa4c9bb9f3fa01588063c03cea00
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2023-03-29 17:57:03 +0200

    feat(EDCO-18): form stories added

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,15 @@
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.png';
+declare module '*.jpg' {
+  const value: string;
+  export = value;
+}
+declare module '*.jpeg' {
+  const value: string;
+  export = value;
+}
+
+declare module '*.png' {
+  const value: string;
+  export = value;
+}
+
 declare module '*.svg';
