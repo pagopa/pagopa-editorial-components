8529ac003c39edac6a2e182a5e5e2860378eb2d9
Vito Falco <vitofalco@gmail.com>
2023-03-30 09:50:32 +0200

    [#EDCO-24]  Code's tuning to publish on npm (#18)
    * Add type to use the mui-italia palette
    
    * Pipeline (WIP)
    
    * Tuning to be more npm friendly (WIP)
    
    * Pipeline fix
    
    * Pipeline tuning
    
    * revert to @pagopa
    
    * Add missing conf

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,3 +2,8 @@ declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.png';
 declare module '*.svg';
+declare module '@pagopa/mui-italia' {
+  import theme from '@pagopa/mui-italia/dist/theme/index';
+
+  export default theme;
+}
