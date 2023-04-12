537f40cbfc0fd7a8794aa3bc88bbf23e6d1fe100
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-04-12 14:50:46 +0200

    fix(EDCO-12): added missing export

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import Download from './components/Download';
 import Feature from './components/Feature/Feature';
 import Hero from './components/Hero';
 import Listing from './components/Listing';
+import Newsroom from './components/Newsroom';
 import PhotoVideo from './components/PhotoVideo';
 import Text from './components/Text';
-import Newsroom from './components/Newsroom';
+import { Abstract } from './components/Abstract';
 import { Accordion } from './components/Accordion/Accordion';
 import { BannerLink } from './components/BannerLink';
 import { Editorial } from './components/Editorial';
@@ -16,6 +17,7 @@ import { PreHeader } from './components/PreHeader';
 import { Stats } from './components/Stats';
 
 export {
+  Abstract,
   Accordion,
   BannerLink,
   Download,
@@ -27,9 +29,9 @@ export {
   HowTo,
   Kpi,
   Listing,
+  Newsroom,
   PhotoVideo,
   PreHeader,
   Stats,
   Text,
-  Newsroom,
 };
