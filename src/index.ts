06c36fe4d54f8e8e9212a3ff73f01c931f34dd18
Michele Moio <michele.moio@protonmail.com>
2023-04-12 09:29:54 +0200

    fix(EDCO-25): components export (#30)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
+import { Accordion } from './components/Accordion/Accordion';
 import { BannerLink } from './components/BannerLink';
 import { Editorial } from './components/Editorial';
+import Feature from './components/Feature/Feature';
 import { Header } from './components/Header';
 import Hero from './components/Hero';
+import { HowTo } from './components/HowTo';
+import PhotoVideo from './components/PhotoVideo';
 
 export default {
+  Accordion,
   BannerLink,
   Editorial,
+  Feature,
   Header,
   Hero,
+  HowTo,
+  PhotoVideo,
 };
