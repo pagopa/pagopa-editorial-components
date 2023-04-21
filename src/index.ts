37100abc26bc3e32370ee214c3483a2e8b6ba170
Michele Moio <michele.moio@protonmail.com>
2023-04-21 11:57:03 +0200

    fix(EDCO-19): missing component export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Accordion } from './components/Accordion/Accordion';
 import { BannerLink } from './components/BannerLink';
 import { Editorial } from './components/Editorial';
 import Feature from './components/Feature/Feature';
+import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header';
 import Hero from './components/Hero';
 import { HowTo } from './components/HowTo';
@@ -12,6 +13,7 @@ export default {
   BannerLink,
   Editorial,
   Feature,
+  Footer,
   Header,
   Hero,
   HowTo,
