30e637207d51acd53171cc3becd0aa4156ad7096
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2023-04-21 09:53:15 +0200

    feat(EDCO-30): after rebase

--- src/components/PhotoVideo/YouTube.tsx
+++ src/components/PhotoVideo/YouTube.tsx
@@ -1,8 +1,9 @@
 import { useRef } from 'react';
 import YouTube, { type YouTubePlayer } from 'react-youtube';
 import { type PhotoVideoTextProps } from './Text';
-import { Box, Link, Container, Grid, Stack } from '@mui/material';
+import { Link, Grid, Stack } from '@mui/material';
 import Text from './Text';
+import EContainer from '../EContainer';
 
 export interface PhotoVideoYouTubeProps extends PhotoVideoTextProps {
   full?: boolean;
@@ -34,68 +35,59 @@ const YouTubeVideo = ({
   };
 
   return (
-    <Box
-      component="section"
-      bgcolor={theme === 'dark' ? 'primary.dark' : 'white'}
+    <EContainer
+      spacing={{ xs: 3, md: 16 }}
       py={7}
+      alignItems="center"
+      direction={reverse ? 'row-reverse' : 'row'}
+      background={theme === 'dark' ? 'primary.dark' : 'white'}
     >
-      <Container maxWidth="lg">
-        <Grid
-          container
-          spacing={{ xs: 3, md: 16 }}
-          alignItems={'center'}
-          direction={reverse ? 'row-reverse' : 'row'}
-        >
-          <Grid item xs={12} md={full ? 12 : 6}>
-            <YouTube
-              videoId={youtubeID}
-              id={`video-${youtubeID}`}
-              className="video__frame"
-              onReady={({ target }) => {
-                videoRef.current = target;
+      <Grid item xs={12} md={full ? 12 : 6}>
+        <YouTube
+          videoId={youtubeID}
+          id={`video-${youtubeID}`}
+          className="video__frame"
+          onReady={({ target }) => {
+            videoRef.current = target;
+          }}
+          style={{
+            aspectRatio: 1.777,
+            borderRadius: '25px',
+            overflow: 'hidden',
+          }}
+          opts={{
+            width: '100%',
+            height: '100%',
+            host: 'https://www.youtube-nocookie.com',
+            playerVars: {
+              autoplay: autoplay ? 1 : 0,
+              showinfo: 0,
+              mute: 1,
+            },
+          }}
+        />
+      </Grid>
+      {!full && (
+        <Grid item md={6}>
+          <Text title={title} subtitle={subtitle} theme={theme} />
+          <Stack direction="row" alignItems="center">
+            <Link
+              component="button"
+              onClick={play}
+              sx={{
+                fontWeight: 700,
+                fontSize: '16px',
+                textDecoration: 'none',
+                color:
+                  theme === 'dark' ? 'primary.contrastText' : 'primary.dark',
               }}
-              style={{
-                aspectRatio: 1.777,
-                borderRadius: '25px',
-                overflow: 'hidden',
-              }}
-              opts={{
-                width: '100%',
-                height: '100%',
-                host: 'https://www.youtube-nocookie.com',
-                playerVars: {
-                  autoplay: autoplay ? 1 : 0,
-                  showinfo: 0,
-                  mute: 1,
-                },
-              }}
-            />
-          </Grid>
-          {!full && (
-            <Grid item md={6}>
-              <Text title={title} subtitle={subtitle} theme={theme} />
-              <Stack direction="row" alignItems="center">
-                <Link
-                  component="button"
-                  onClick={play}
-                  sx={{
-                    fontWeight: 700,
-                    fontSize: '16px',
-                    textDecoration: 'none',
-                    color:
-                      theme === 'dark'
-                        ? 'primary.contrastText'
-                        : 'primary.dark',
-                  }}
-                >
-                  {playButtonLabel}
-                </Link>
-              </Stack>
-            </Grid>
-          )}
+            >
+              {playButtonLabel}
+            </Link>
+          </Stack>
         </Grid>
-      </Container>
-    </Box>
+      )}
+    </EContainer>
   );
 };
 
