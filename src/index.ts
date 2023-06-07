d48749c2292f9dea331eda8ac651d7df20dd4afe
Strato Ivan Di Luccio <34749257+stratoivandiluccio@users.noreply.github.com>
2023-06-07 09:41:40 +0200

    feat(EDCO-48): listing component added to the library

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Feature from './components/Feature/Feature';
 import Hero from './components/Hero';
 import PhotoVideo from './components/PhotoVideo';
+import Listing from './components/Listing';
 import Text from './components/Text';
 import { Accordion } from './components/Accordion/Accordion';
 import { BannerLink } from './components/BannerLink';
@@ -21,5 +22,6 @@ export {
   HowTo,
   PhotoVideo,
   PreHeader,
+  Listing,
   Text,
 };
