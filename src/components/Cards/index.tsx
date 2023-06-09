0e079458f96b8005e54b459539dffd8007c718d3
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2023-06-09 16:41:06 +0200

    feat(EDCO-07): cards component, firs version

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,15 +1,69 @@
-import { Grid, Typography } from '@mui/material';
-import { type Generic, type CommonProps } from '../../types/components';
+import {
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+  Stack,
+  Link,
+} from '@mui/material';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import { type CommonProps, type Generic } from '../../types/components';
 import EContainer from '../EContainer';
-import Item, { type IItem } from './item';
+import { EIcon, type EIconProps } from '../EIcon';
+
 import { isJSX } from '../../utils';
-import { type ReactNode } from 'react';
 
 const layoutMap = new Map();
 
-layoutMap.set('3-items', 3);
-layoutMap.set('4-items', 4);
-layoutMap.set('full-text', 2);
+layoutMap.set('3-items', 4);
+layoutMap.set('4-items', 3);
+layoutMap.set('full-text', 6);
+
+interface IItem {
+  icon?: Generic | EIconProps['name'];
+  label?: string;
+  title: string;
+  text: string;
+  link?: {
+    href: string;
+    text: string;
+    title: string;
+  };
+}
+
+const Item = (props: IItem) => {
+  const { title, text, icon, link } = props;
+  return (
+    <Card elevation={16}>
+      <CardContent>
+        {isJSX(icon) ? icon : <EIcon name={icon} />}
+        <Typography variant="h6">{title}</Typography>
+        <Typography variant="body1">{text} </Typography>
+        {link && (
+          <Stack
+            mt={2}
+            direction="row"
+            alignItems="center"
+            color="primary.main"
+          >
+            <Link
+              color="primary.main"
+              underline="none"
+              textTransform="capitalize"
+              href={link.href}
+              title={link.title}
+              fontSize={14}
+              fontWeight={400}
+            >
+              {link.text}
+            </Link>
+            <ArrowRightAltIcon sx={{ color: 'inerith', fontSize: 18 }} />
+          </Stack>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
 
 export interface CardsProps extends CommonProps {
   items: IItem[];
@@ -17,50 +71,20 @@ export interface CardsProps extends CommonProps {
   text: {
     title: string;
     subtitle?: string;
-    body?: string | Generic;
+    body?: string;
   };
 }
 
-const ItemsContainer = ({ children }: { children: ReactNode[] }) => (
-  <Grid container spacing={2} justifyContent="center">
-    {children}
-  </Grid>
-);
-
 const Cards = (props: CardsProps) => {
-  const { items, layout, theme, text } = props;
-  const background = theme === 'dark' ? 'primary.dark' : 'background.paper';
-  const textColor = theme === 'dark' ? 'primary.contrastText' : 'text.primary';
+  const { items, layout } = props;
   return (
-    <EContainer background={background} py={8} spacing={{ md: '145px' }}>
-      {layout === 'full-text' && (
-        <Grid item md={4} color={textColor}>
-          <Typography variant="h2" mb={5} color={'inherit'}>
-            {text.title}
-          </Typography>
-          <Typography variant="h6" mb={5} color={'inherit'}>
-            {text.subtitle}
-          </Typography>
-          {isJSX(text.body) ? (
-            text.body
-          ) : (
-            <Typography variant="body1" color={'inherit'}>
-              {text.body}
-            </Typography>
-          )}
-        </Grid>
-      )}
-      <Grid item md={layout !== 'full-text' ? 12 : 8}>
-        <ItemsContainer>
-          {items.map((item, i) => (
-            <Item
-              key={`${item.title}-${i}`}
-              {...item}
-              layout={layout}
-              textAlign={layout !== 'full-text' ? 'center' : 'left'}
-            />
-          ))}
-        </ItemsContainer>
+    <EContainer background="background.paper" py={8}>
+      <Grid container spacing={2}>
+        {items.map((item, i) => (
+          <Grid item key={`${item.title}-${i}`} md={layoutMap.get(layout)}>
+            <Item {...item} />
+          </Grid>
+        ))}
       </Grid>
     </EContainer>
   );
