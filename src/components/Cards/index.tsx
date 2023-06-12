021db199ea8ba59d16b8afda82ed0455e65050a6
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2023-06-12 16:52:44 +0200

    feat(EDCO-07): cards component, responsive improved

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -4,6 +4,7 @@ import { type Generic, type CommonProps } from '../../types/components';
 import EContainer from '../EContainer';
 import Item, { type IItem } from './item';
 import { isJSX } from '../../utils';
+import { type ReactNode } from 'react';
 
 const layoutMap = new Map();
 
@@ -21,41 +22,60 @@ export interface CardsProps extends CommonProps {
   };
 }
 
+const ItemsContainer = ({
+  layout,
+  children,
+}: {
+  layout: CardsProps['layout'];
+  children: ReactNode[];
+}) => {
+  const isMasonry = layout === 'full-text';
+
+  return isMasonry ? (
+    <Masonry columns={{ md: 2 }} spacing={4}>
+      {children}
+    </Masonry>
+  ) : (
+    <Grid container spacing={2} justifyContent="center">
+      {children}
+    </Grid>
+  );
+};
+
 const Cards = (props: CardsProps) => {
   const { items, layout, theme, text } = props;
   const background = theme === 'dark' ? 'primary.dark' : 'background.paper';
   const textColor = theme === 'dark' ? 'primary.contrastText' : 'text.primary';
   return (
-    <EContainer background={background} py={8}>
-      <Grid container spacing="145px">
-        {layout === 'full-text' && (
-          <Grid item md={4} color={textColor}>
-            <Typography variant="h2" mb={5} color={'inherit'}>
-              {text.title}
+    <EContainer background={background} py={8} spacing="145px">
+      {layout === 'full-text' && (
+        <Grid item md={4} color={textColor}>
+          <Typography variant="h2" mb={5} color={'inherit'}>
+            {text.title}
+          </Typography>
+          <Typography variant="h6" mb={5} color={'inherit'}>
+            {text.subtitle}
+          </Typography>
+          {isJSX(text.body) ? (
+            text.body
+          ) : (
+            <Typography variant="body1" color={'inherit'}>
+              {text.body}
             </Typography>
-            <Typography variant="h6" mb={5} color={'inherit'}>
-              {text.subtitle}
-            </Typography>
-            {isJSX(text.body) ? (
-              text.body
-            ) : (
-              <Typography variant="body1" color={'inherit'}>
-                {text.body}
-              </Typography>
-            )}
-          </Grid>
-        )}
-        <Grid item md={layout !== 'full-text' ? 12 : 8}>
-          <Masonry columns={layoutMap.get(layout)} spacing={4}>
-            {items.map((item, i) => (
-              <Item
-                key={`${item.title}-${i}`}
-                {...item}
-                textAlign={layout !== 'full-text' ? 'center' : 'left'}
-              />
-            ))}
-          </Masonry>
+          )}
         </Grid>
+      )}
+      <Grid item md={layout !== 'full-text' ? 12 : 8}>
+        <ItemsContainer layout={layout}>
+          {items.map((item, i) => (
+            <Item
+              key={`${item.title}-${i}`}
+              {...item}
+              layout={layout}
+              textAlign={layout !== 'full-text' ? 'center' : 'left'}
+            />
+          ))}
+        </ItemsContainer>
       </Grid>
     </EContainer>
   );
