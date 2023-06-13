15057634ecda1d4d48e232a6262f30a3faad0b3d
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2023-06-13 11:18:09 +0200

    feat(EDCO-13): stripe link, a small review

--- src/components/StripeLink.tsx
+++ src/components/StripeLink.tsx
@@ -1,5 +1,5 @@
 import {
-  Box,
+  Grid,
   Button,
   Stack,
   Typography,
@@ -8,6 +8,7 @@ import {
 } from '@mui/material';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { type Generic } from 'types/components';
+import EContainer from './EContainer';
 
 export interface StripeLinkProps {
   theme: 'dark' | 'light';
@@ -24,32 +25,33 @@ const StripeLink = (props: StripeLinkProps) => {
   const bgButton = theme === 'light' ? 'background.paper' : 'primary.main';
   const underMd = useMediaQuery(breakpoints.down('md'));
   return (
-    <Box bgcolor={themeStyleBg} maxWidth={'1440px'} p={2}>
-      <Stack
-        direction={{ md: 'row', xs: 'column' }}
-        justifyContent={{ md: 'flex-start' }}
-        spacing={3}
-        alignItems={{ md: 'center', xs: 'flex-start' }}
-        sx={{ ml: { md: 16 } }}
-      >
-        {icon && !underMd && icon}
-        <Typography variant="body2" color={'background.paper'}>
-          {subtitle}
-        </Typography>
-        <Button
-          variant="contained"
-          size="small"
-          sx={{
-            backgroundColor: bgButton,
-            color: themeStyle,
-            ':hover': { backgroundColor: bgButton },
-          }}
-          endIcon={<ArrowForwardIcon color="inherit"></ArrowForwardIcon>}
+    <EContainer background={themeStyleBg} py={2}>
+      <Grid item>
+        <Stack
+          direction={{ md: 'row', xs: 'column' }}
+          justifyContent={{ md: 'flex-start' }}
+          spacing={3}
+          alignItems={{ md: 'center', xs: 'flex-start' }}
         >
-          {buttonText}
-        </Button>
-      </Stack>
-    </Box>
+          {icon && !underMd && icon}
+          <Typography variant="body2" color={'background.paper'}>
+            {subtitle}
+          </Typography>
+          <Button
+            variant="contained"
+            size="small"
+            sx={{
+              backgroundColor: bgButton,
+              color: themeStyle,
+              ':hover': { backgroundColor: bgButton },
+            }}
+            endIcon={<ArrowForwardIcon color="inherit"></ArrowForwardIcon>}
+          >
+            {buttonText}
+          </Button>
+        </Stack>
+      </Grid>
+    </EContainer>
   );
 };
 
