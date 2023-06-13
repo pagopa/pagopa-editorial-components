6e910f0fbb155a2cde1e245f390ac4bda11147dc
Strato Ivan Di Luccio <34749257+stratoivandiluccio@users.noreply.github.com>
2023-06-13 15:59:12 +0200

    feat(EDCO-07): Cards component  added to the library
    * feat(EDCO-07): cards component, firs version
    
    * feat(EDCO-07): cards components, layouts added
    
    * feat(EDCO-07): added more style to cards component
    
    * feat(EDCO-07): cards component, added more complete stories
    
    * feat(EDCO-07): cards component, responsive improved
    
    * feat(EDCO-07): cards component, dinamic spacing
    
    * feat(EDCO-07): masonry spacing bug fixed
    
    * feat(EDCO-07): masonry effect deleted
    
    * feat(EDCO-07): masonory effect deleted

---
+++ src/components/Cards/index.tsx
@@ -0,0 +1,69 @@
+import { Grid, Typography } from '@mui/material';
+import { type Generic, type CommonProps } from '../../types/components';
+import EContainer from '../EContainer';
+import Item, { type IItem } from './item';
+import { isJSX } from '../../utils';
+import { type ReactNode } from 'react';
+
+const layoutMap = new Map();
+
+layoutMap.set('3-items', 3);
+layoutMap.set('4-items', 4);
+layoutMap.set('full-text', 2);
+
+export interface CardsProps extends CommonProps {
+  items: IItem[];
+  layout: '3-items' | '4-items' | 'full-text';
+  text: {
+    title: string;
+    subtitle?: string;
+    body?: string | Generic;
+  };
+}
+
+const ItemsContainer = ({ children }: { children: ReactNode[] }) => (
+  <Grid container spacing={2} justifyContent="center">
+    {children}
+  </Grid>
+);
+
+const Cards = (props: CardsProps) => {
+  const { items, layout, theme, text } = props;
+  const background = theme === 'dark' ? 'primary.dark' : 'background.paper';
+  const textColor = theme === 'dark' ? 'primary.contrastText' : 'text.primary';
+  return (
+    <EContainer background={background} py={8} spacing={{ md: '145px' }}>
+      {layout === 'full-text' && (
+        <Grid item md={4} color={textColor}>
+          <Typography variant="h2" mb={5} color={'inherit'}>
+            {text.title}
+          </Typography>
+          <Typography variant="h6" mb={5} color={'inherit'}>
+            {text.subtitle}
+          </Typography>
+          {isJSX(text.body) ? (
+            text.body
+          ) : (
+            <Typography variant="body1" color={'inherit'}>
+              {text.body}
+            </Typography>
+          )}
+        </Grid>
+      )}
+      <Grid item md={layout !== 'full-text' ? 12 : 8}>
+        <ItemsContainer>
+          {items.map((item, i) => (
+            <Item
+              key={`${item.title}-${i}`}
+              {...item}
+              layout={layout}
+              textAlign={layout !== 'full-text' ? 'center' : 'left'}
+            />
+          ))}
+        </ItemsContainer>
+      </Grid>
+    </EContainer>
+  );
+};
+
+export default Cards;
---
+++ src/stories/Cards/ligth.stories.tsx
@@ -0,0 +1,144 @@
+import { type ComponentMeta, type ComponentStory } from '@storybook/react';
+import Cards from '../../components/Cards';
+
+export default {
+  component: Cards,
+  title: 'Cards/ligth',
+} as ComponentMeta<typeof Cards>;
+
+const Template: ComponentStory<typeof Cards> = (args) => <Cards {...args} />;
+
+export const ThreeColumnLayout = Template.bind({});
+ThreeColumnLayout.args = {
+  theme: 'light',
+  text: {
+    title: 'lorem ipsum',
+  },
+  layout: '3-items',
+  items: [
+    {
+      icon: 'Air',
+      title: 'Title',
+      text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+      link: {
+        href: 'https://google.com',
+        title: 'vai al sito',
+        text: 'leggi',
+      },
+    },
+    {
+      icon: 'AccessAlarm',
+      title: 'Title',
+      text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+      link: {
+        href: 'https://google.com',
+        title: 'vai al sito',
+        text: 'leggi',
+      },
+    },
+    {
+      icon: 'AddReaction',
+      title: 'Title',
+      text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+      link: {
+        href: 'https://google.com',
+        title: 'vai al sito',
+        text: 'leggi',
+      },
+    },
+  ],
+};
+
+export const FourColumnLayout = Template.bind({});
+FourColumnLayout.args = {
+  theme: 'light',
+  text: {
+    title: 'lorem ipsum',
+  },
+  layout: '4-items',
+  items: [
+    {
+      label: 'label',
+      title: 'Title',
+      text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    },
+    {
+      label: 'label',
+      title: 'Title',
+      text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    },
+    {
+      label: 'label',
+      title: 'Title',
+      text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    },
+    {
+      label: 'label',
+      title: 'Title',
+      text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    },
+  ],
+};
+
+export const WithText = Template.bind({});
+WithText.args = {
+  theme: 'light',
+  text: {
+    title: 'lorem ipsum',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    body: (
+      <p style={{ fontSize: '16px', fontWeight: '400' }}>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
+        dis <strong>parturient</strong> montes, nascetur ridiculus mus.
+        <br />
+        <br /> Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
+        sem. Nulla consequat <strong>massa</strong> quis enim. Donec pede justo,
+        fringilla vel, aliquet nec, vulputate eget, arcu.
+      </p>
+    ),
+  },
+  layout: 'full-text',
+  items: [
+    {
+      icon: 'AccessAlarm',
+      title: 'Title',
+      text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+      link: {
+        href: 'https://google.com',
+        title: 'vai al sito',
+        text: 'leggi',
+      },
+    },
+    {
+      icon: 'Accessibility',
+      title: 'Title',
+      text: 'Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. ',
+      link: {
+        href: 'https://google.com',
+        title: 'vai al sito',
+        text: 'leggi',
+      },
+    },
+    {
+      icon: 'Air',
+      title: 'Title',
+      text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+      link: {
+        href: 'https://google.com',
+        title: 'vai al sito',
+        text: 'leggi',
+      },
+    },
+    {
+      icon: 'AddReaction',
+      title: 'Title',
+      text: 'Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. ',
+      link: {
+        href: 'https://google.com',
+        title: 'vai al sito',
+        text: 'leggi',
+      },
+    },
+  ],
+};
