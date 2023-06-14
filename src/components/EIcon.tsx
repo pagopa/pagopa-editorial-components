02ed1b7c3404d7445fad281b1f71e6ea68928480
Michele Moio <michele.moio@protonmail.com>
2023-06-14 14:39:49 +0200

    feat(EDCO-45): EIcon Component
    - Component icon prop can be a JSX.Element or a MuiIcon name
    - EIcon accep all props accepted by Mui SvgIconProp
    - Eicon color can be any string, is passed to a Typography and inherited
      by the icon prop, be it a JSX.Element or a mui icon name

--- src/components/EIcon.tsx
+++ src/components/EIcon.tsx
@@ -1,11 +1,26 @@
 import * as MuiIcons from '@mui/icons-material';
-import { type SvgIconProps } from '@mui/material';
+import { Typography, type SvgIconProps } from '@mui/material';
+import { isJSX } from '../utils';
+import { type Generic } from '../types/components';
+import React from 'react';
 
-export interface EIconProps extends SvgIconProps {
-  name?: keyof typeof MuiIcons;
+export interface EIconProps extends Omit<SvgIconProps, 'color'> {
+  icon?: keyof typeof MuiIcons | Generic;
+  color?: string;
 }
 
-export const EIcon = ({ name, ...rest }: EIconProps) => {
-  const Icon = name && MuiIcons[name];
-  return Icon ? <Icon {...rest} /> : null;
+export const EIcon = (props: EIconProps) => {
+  const IconWrapper = ({ icon, ...rest }: EIconProps) => {
+    if (isJSX(icon))
+      return React.cloneElement(icon, { ...rest, ...icon.props });
+
+    const Icon = icon && MuiIcons[icon];
+    return Icon ? <Icon {...rest} color="inherit" /> : null;
+  };
+
+  return (
+    <Typography color={props.color}>
+      <IconWrapper color="inherit" {...props} />
+    </Typography>
+  );
 };
