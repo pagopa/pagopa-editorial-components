99a4e877a9ab0a1dceb634f0bc8e8d2f36ee7e49
Anis Lucidi <anis.lucidi@pagopa.it>
2023-11-29 12:09:25 +0100

    test: added unit test for accordion

--- src/components/Accordion/Accordion.test.tsx
+++ src/components/Accordion/Accordion.test.tsx
@@ -1,4 +1,5 @@
 import { Accordion } from './Accordion';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 /**
  * @jest-environment jsdom
@@ -100,4 +101,51 @@ it('renders correctly with all props and two accordion items dark', () => {
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('opens the accordion when the button is clicked and closes it', async () => {
+  render(
+    <Accordion
+      title="titleTest"
+      subtitle="subtitleTest"
+      description="descriptionTest"
+      accordionItems={[{ header: 'tetsHead', content: 'testCont' }]}
+      theme="dark"
+    />
+  );
+  const accordionContent = screen.queryByText('testCont');
+  const button = screen.getByRole('button');
+
+  await waitFor(() => {
+    expect(accordionContent).not.toBeVisible();
+  });
+
+  fireEvent.click(button);
+
+  await waitFor(() => {
+    expect(accordionContent).toBeVisible();
+  });
+
+  fireEvent.click(button);
+
+  await waitFor(() => {
+    expect(accordionContent).not.toBeVisible();
+  });
+});
+
+it('renders text correctly', async () => {
+  const { getByText } = render(
+    <Accordion
+      title="titleTest"
+      subtitle="subtitleTest"
+      description="descriptionTest"
+      accordionItems={[{ header: 'tetsHead', content: 'testCont' }]}
+      theme="dark"
+    />
+  );
+  expect(getByText('titleTest')).toBeInTheDocument();
+  expect(getByText('subtitleTest')).toBeInTheDocument();
+  expect(getByText('descriptionTest')).toBeInTheDocument();
+  expect(getByText('tetsHead')).toBeInTheDocument();
+  expect(getByText('testCont')).toBeInTheDocument();
 });
