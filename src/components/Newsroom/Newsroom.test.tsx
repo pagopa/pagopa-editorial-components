4baf13c9612c354ed94935d33520220f523f3285
Anis Lucidi <anis.lucidi@pagopa.it>
2023-12-01 10:49:57 +0100

    test: added unit test for newsroom

--- src/components/Newsroom/Newsroom.test.tsx
+++ src/components/Newsroom/Newsroom.test.tsx
@@ -1,5 +1,6 @@
 import Newsroom from './index';
 import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
 /**
  * @jest-environment jsdom
  */
@@ -100,4 +101,117 @@ it('renders correctly standard ', () => {
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test('All links are rendered correctly', async () => {
+  const { getAllByText } = render(
+    <Newsroom
+      items={[
+        {
+          date: {
+            date: new Date('2023-11-23T11:35:50.763Z'),
+          },
+          href: {
+            label: 'leggi',
+            link: 'https://google1.it',
+            title: 'vai al sito',
+          },
+          img: {
+            alt: 'A picture showing some books',
+            src: 'static/media/newsroom-cover.59549252.png',
+          },
+          title:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. ',
+        },
+        {
+          date: {
+            date: new Date('2023-11-23T11:35:50.763Z'),
+          },
+          href: {
+            label: 'leggi',
+            link: 'https://google2.it',
+            title: 'vai al sito',
+          },
+          img: {
+            alt: 'A picture showing some books',
+            src: 'static/media/newsroom-cover.59549252.png',
+          },
+          title:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. ',
+        },
+        {
+          date: {
+            date: new Date('2023-11-23T11:35:50.763Z'),
+          },
+          href: {
+            label: 'leggi',
+            link: 'https://google3.it',
+            title: 'vai al sito',
+          },
+          img: {
+            alt: 'A picture showing some books',
+            src: 'static/media/newsroom-cover.59549252.png',
+          },
+          title:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. ',
+        },
+        {
+          date: {
+            date: new Date('2023-11-23T11:35:50.763Z'),
+          },
+          href: {
+            label: 'leggi',
+            link: 'https://google4.it',
+            title: 'vai al sito',
+          },
+          img: {
+            alt: 'A picture showing some books',
+            src: 'static/media/newsroom-cover.59549252.png',
+          },
+          title:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. ',
+        },
+        {
+          date: {
+            date: new Date('2023-11-23T11:35:50.763Z'),
+          },
+          href: {
+            label: 'leggi',
+            link: 'https://google5.it',
+            title: 'vai al sito',
+          },
+          img: (
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor.
+            </div>
+          ),
+          title:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. ',
+        },
+      ]}
+    />
+  );
+  expect(getAllByText('leggi')[0]?.closest('a')).toHaveAttribute(
+    'href',
+    'https://google1.it'
+  );
+  expect(getAllByText('leggi')[1]?.closest('a')).toHaveAttribute(
+    'href',
+    'https://google2.it'
+  );
+  expect(getAllByText('leggi')[2]?.closest('a')).toHaveAttribute(
+    'href',
+    'https://google3.it'
+  );
+  expect(getAllByText('leggi')[3]?.closest('a')).toHaveAttribute(
+    'href',
+    'https://google4.it'
+  );
+  expect(getAllByText('leggi')[4]?.closest('a')).toHaveAttribute(
+    'href',
+    'https://google5.it'
+  );
 });
